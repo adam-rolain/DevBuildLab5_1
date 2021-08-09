5c5f30ab5e503410676da3a75932e1aeacafa6ff
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-08-09 14:46:11 -0400

    Intializing repo

---
+++ Lab5_1RPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5_1RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
